f8a5af67a9c5da68a3dadf3641373387151b5b33
women-life-freedom-iran <115878896+women-life-freedom-iran@users.noreply.github.com>
2022-10-18 23:49:05 +0200

    Update data.js

--- data.js
+++ data.js
@@ -3,21 +3,21 @@ var data = [
     title: "For Sheen Sheen Abad girls",
     content:
       "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
+    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
+    link: "https://www.tondar.ca/public_html/news/10642/",
   },
   {
     title: "For Homa Darabi",
     content:
       "Homa was an Iranian pediatrician and professor of child psychiatry at the University of Tehran. She was dismissed from her position for non-adherence to hijab in December 1991. As a sign of protest, Homa immolated herself by pouring petrol over her head on 21 February 1994, after she had taken her hijab off on a public thoroughfare. She died of the burns in a hospital the next day.",
-    image: "https://images.app.goo.gl/7kYHetF4N8iyyrbj9",
+    image: "https://pbs.twimg.com/media/FMKKO6nVgAA9Ppd.jpg",
     link: "https://www.ontheissuesmagazine.com/1994fall/tehran.php",
   },
   {
     title: "For Lake Urmia",
     content:
       "Lake Urmia was the largest lake in the Middle East and the sixth-largest saltwater lake on Earth, with a surface area of approximately 5,200 km2 (2,000 sq mi). By late 2017, the lake had shrunk to 10% of its former size due to massive water mismanagement, including damming the local rivers that flow into it and pumping groundwater from the surrounding area. Lake Urmia is now almost dried up completely.",
-    image: "https://images.app.goo.gl/AxV64RLTbjqxTc4c6",
+    image: "https://caspiannews.com/media/caspian_news/all_original_photos/1520341334_747449_1520341121_6815877earthobservatory_nasa_gov.jpg",
     link: "https://www.iranintl.com/en/202208165787",
   },
   {
@@ -31,7 +31,7 @@ var data = [
     title: "For Mohsen Mohammad-Pour",
     content:
       "Mohsen was a construction worker and supported his family. The Islamic Republic government killed Mohsen in 2019 protests in Khoramshahr. He was 16.",
-    image: "https://images.app.goo.gl/YtxtiktdPzKn9U5B7",
+    image: "https://pbs.twimg.com/media/EL5rCJVWkAAUIzQ.jpg",
     link: "https://twitter.com/IranHrm/status/1206519446719909889?s=20&t=iThiW-aIkkRTbT7nutFHDw",
   },
   {
@@ -45,149 +45,81 @@ var data = [
     title: "For Kooye Daneshgah students",
     content:
       "The protests began on July 9, 1999 through a peaceful demonstration by a group of students of Tehran University against the closure of a newspaper. Plainclothes paramilitaries descended on a university dormitory. At least 7 young lives were taken, 1 person lost his eyes, 2 people disappeared, and many others were injured. Ezatollah Ebrahim-Nejad, Saeid Zeynali, Fereshteh Alizadeh, Tami Hamifar, and … We never knew all their names.",
-    image: "https://images.app.goo.gl/cmDADqKmRLZy4Brr6",
+    image: "https://iranianuk.com/db/pages/2005/images/pic19/4277928.jpg",
     link: "https://www.brookings.edu/blog/markaz/2014/07/10/fifteen-years-after-the-18th-of-tir-the-legacy-of-student-protests-that-shook-iran/",
   },
   {
     title: "For Karun Hajizadeh",
     content:
       "Karun was the son of Hamid Hajizadeh, a poet and an author. During the serial murders that targeted intellectuals and writers in 1998, Karun was killed by the Islamic Republic with multiple stab wounds along with his father, Hamid. Karun was 9 years old.",
-    image: "https://images.app.goo.gl/1G4VVXpwzuwCrbPu8",
+    image: "https://gdb.rferl.org/a472ecb0-6862-41d9-8c11-c5fdd20e610b_w408_r1_s.jpg",
     link: "https://www.rferl.org/a/two-decades-later-still-no-justice-for-iran-chain-murders-of-intellectuals/30997536.html",
   },
   {
     title: "For Saeid Zeynali",
     content:
       "Akram Neghabi’s son, Saeed Zeynali, disappeared during the Kooye Daneshgah attack in 1999. After 23 years, Akram is still asking “where is my Saeid?” and gets no answer.",
-    image: "https://images.app.goo.gl/Rmb24fzcjAEqvHE37",
+    image: "https://ichef.bbci.co.uk/news/640/cpsprodpb/15404/production/_119344078__107818939_e8921e25-c2fe-4982-9b70-1c3e24fe15ed.jpg",
     link: "",
   },
   {
-    title: "Sheen Sheen4 Abad girls",
+    title: "For Sattar Beheshti",
     content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
+      "He was a worker and blogger who was arrested by Islamic Republic’s Cyber police in 2013 for criticizing the government on Facebook. Sattar died under torture while in custody. He was 35.",
+    image: "https://iranhumanrights.org/wp-content/uploads/satar.jpg",
+    link: "https://en.wikipedia.org/wiki/Sattar_Beheshti",
   },
   {
-    title: "Sheen Sheen4 Abad girls",
+    title: "For Mohammad Musavi-Zadeh",
     content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
+      "Millions of Iranian students couldn’t afford to buy devices to attend online classes during Covid. Mohammad was one of those. He committed suicide due to his family's inability to buy a phone. He was 11.",
+    image: "https://static.cdn.asset.aparat.com/avt/25988496-2065-b__6960.jpg",
+    link: "https://www.ncr-iran.org/en/ncri-statements/statement-human-rights/iran-11-year-old-student-commits-suicide-a-reflection-of-the-clerical-regimes-plundering-and-criminal-policies/",
   },
   {
-    title: "Sheen Sheen4 Abad girls",
+    title: "For Farhad Khosravi",
     content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
+      "Unemployment has made many people carry heavy goods (i.e. a fridge) across the border on their shoulders to earn some money. They are called “Koolbar”s. Farhad was one of them. His frozen body was found after three days with clenched fists. Many other Koolbars get shot by the border police every year. People carried pieces of bread in Farhad’s funeral, as he was dead on a frozen trail to not die of hunger. Farhad was only 14.",
+    image: "https://gdb.rferl.org/249B2C5A-E910-4C29-9F9C-B827495B3F50_cx0_cy11_cw0_w1023_r1_s.jpg",
+    link: "https://en.radiofarda.com/a/children-working-as-porters-killed-by-guards-on-iran-iraq-border/30338407.html",
   },
   {
-    title: "Sheen Sheen4 Abad girls",
+    title: "For Pooya Bakhtyari",
     content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
+      "Pooya was 27 years old when he was killed in the November 2019 protests. In his last video on Instagram, he filmed the sunset in a protest as he was describing his passion for liberty. He was shot in the skull a few hours later. He was vegan.",
+    image: "https://gdb.rferl.org/D69BC934-FBC3-4C4E-8930-A4150201683B_cx13_cy52_cw76_w408_r1.jpg",
+    link: "https://en.wikipedia.org/wiki/Pouya_Bakhtiari",
   },
   {
-    title: "Sheen Sheen4 Abad girls",
+    title: "For firefighters of the Plasko Building",
     content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
+      "The fire and collapse of the Plasko Building (a commercial 17-floor building in Tehran) in 2017 took 3 hours. Prior to this event, the Tehran firefighting department had asked for new equipment, which the government denied. The building didn’t have a working fire alarm system in place. Despite all of this, those firefighters fought with all they had and put their life into this. The official reports indicate that 20 people died, 16 of whom were firefighters. Neither those firefighters, nor those working in Plasco had the privilege of working in a safe environment with proper safety equipment..",
+    image: "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2017_03/1870971/170119-tehran-fire-cr_11.jpg",
+    link: "https://en.wikipedia.org/wiki/Plasco_Building",
   },
   {
-    title: "Sheen Sheen4 Abad girls",
+    title: "For Zahra Sedighi and Elham Choobdar",
     content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
+      "Sareh and Elham were two LGBTQ+ activists sentenced to death in September 2022 by the Islamic Republic for “encouraging homosexuality”. They have been arrested in Iraq and Turkey, while they were trying to stay safe by living outside of Iran. They are currently imprisoned.",
+    image: "https://taz.de/picture/5796871/948/31065658-1.jpg",
+    link: "https://iranhr.net/en/articles/5460/",
   },
   {
-    title: "Sheen Sheen4 Abad girls",
+    title: "For Sahar Khodayari",
     content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
+      "In March 2019, as a fan of Esteghlal F.C (soccer team in Iran), Sahar attempted to enter Azadi Stadium disguised as a man to watch a match played by the team, against the national ban on women at such events. A few months later, she was told by the Islamic Revolutionary Court of Tehran that she might face a six-month prison sentence. After leaving court, she committed suicide through self-immolation in front of the building. She died a week later of her injuries in August 2019.",
+    image: "https://asset-a.grid.id/crop/0x0:0x0/360x240/photo/2019/09/16/860665707.jpg",
+    link: "https://en.wikipedia.org/wiki/Death_of_Sahar_Khodayari",
   },
+
   {
-    title: "Sheen Sheen4 Abad girls",
+    title: "For Haaleh Sahabi",
     content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
+      "Haaleh was an Iranian humanitarian and a campaigner for women’s rights. She was the daughter of former Iranian MP and veteran opposition figure Ezzatollah Sahabi. On 5 August 2009 she was arrested along with others in front of the parliament for their peaceful demonstration during President Mahmoud Ahmadinejad's second inauguration. She was sentenced to two years in prison but given temporary release for her father's funeral in May 2011. The officials tried to stop the funeral- that made the atmosphere very agitated, ... Finally they forcefully grabbed [my grandfather’s dead] body and took it away. Then my mother fell down and became unconscious. The doctor told us she died because of a heart attack.” her son said. ",
+    image: "https://i.guim.co.uk/img/static/sys-images/Guardian/About/General/2011/6/2/1307037223338/Haleh-Sahabi-007.jpg?width=465&quality=85&dpr=1&s=none",
+    link: "https://en.wikipedia.org/wiki/Haleh_Sahabi",
   },
-  {
-    title: "Sheen Sheen4 Abad girls",
-    content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-  },
-  {
-    title: "Sheen Sheen4 Abad girls",
-    content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-  },
-  {
-    title: "Sheen Sheen4 Abad girls",
-    content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-  },
-  {
-    title: "Sheen Sheen4 Abad girls",
-    content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-  },
-  {
-    title: "Sheen Sheen4 Abad girls",
-    content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-  },
-  {
-    title: "Sheen Sheen4 Abad girls",
-    content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-  },
-  {
-    title: "Sheen Sheen4 Abad girls",
-    content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-  },
-  {
-    title: "Sheen Sheen4 Abad girls",
-    content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-  },
-  {
-    title: "Sheen Sheen4 Abad girls",
-    content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-  },
-  {
-    title: "Sheen Sheen4 Abad girls",
-    content:
-      "Their rural public school was on fire due to the failure of old, non-maintained heating equipment in 2011. 29 students out of 37 suffered severe burns all over their bodies, including their faces. Two of them, Seeran Yeganeh and Sarya Rasul-Zadeh, didn't survive. They received no financial aid for their treatments. The other 29 names were never published.",
-    image: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-    link: "https://media.hamshahrionline.ir/d/2020/12/05/4/4524380.jpg",
-  },
+ 
 ];
 
 var item = data[Math.floor(Math.random() * data.length)];
